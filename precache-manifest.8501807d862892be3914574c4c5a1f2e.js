812d601631d4e40fcfb70d092b3ff5b8a7a981a2
Valery Koshkin <valkoshkin99@gmail.com>
2020-10-19 02:54:29 +0400

    Updates

---
+++ precache-manifest.8501807d862892be3914574c4c5a1f2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a87d336490454f5bd54dbd0258cd7ec",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "673a74f0f9b9253ef0f4",
+    "url": "/react-weather-app/static/css/main.d4f70059.chunk.css"
+  },
+  {
+    "revision": "28d157852d1e674502cf",
+    "url": "/react-weather-app/static/js/2.c33a4b1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-weather-app/static/js/2.c33a4b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673a74f0f9b9253ef0f4",
+    "url": "/react-weather-app/static/js/main.42abbddd.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  },
+  {
+    "revision": "03d01323b71e1a03960f89f2a61f104a",
+    "url": "/react-weather-app/static/media/background.03d01323.jpg"
+  },
+  {
+    "revision": "ccc14ef32b28360e5271882e25d09cc0",
+    "url": "/react-weather-app/static/media/chance-of-rain.ccc14ef3.svg"
+  },
+  {
+    "revision": "c97d3667483e217ac0726e9428077fd2",
+    "url": "/react-weather-app/static/media/wind-speed.c97d3667.svg"
+  }
+]);

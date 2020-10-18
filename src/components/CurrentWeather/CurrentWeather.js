6ca1061c2976cd9237aeb9959a78602b12ee376c
Valery Koshkin <valkoshkin99@gmail.com>
2020-10-18 22:00:48 +0400

    Изменен стиль вывода текущей погоды

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,27 +1,9 @@
 import React from "react";
 import "./CurrentWeather.css";
-import { LocationOnOutlined } from "@material-ui/icons";
+import { LocationOnTwoTone } from "@material-ui/icons";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: "20px",
-    marginTop: "30px",
-    marginBottom: "30px",
-    borderRadius: "10px",
-    minWidth: "300px",
-    maxWidth: "50%",
-    cursor: "default",
-    userSelect: "none",
-    "&:hover": {
-      boxShadow: "0 1px 6px 2px rgba(0, 0, 0, 0.25)",
-      transition: "box-shadow 100ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-    },
-  },
   icon: {
     marginRight: "10px",
     color: "#162635",
@@ -32,9 +14,9 @@ export const CurrentWeather = (props) => {
   const styles = useStyles();
 
   return (
-    <Paper className={styles.root}>
+    <div className="current-weather">
       <div className="current-weather-location">
-        <LocationOnOutlined className={styles.icon} fontSize="large" />
+        <LocationOnTwoTone className={styles.icon} fontSize="large" />
         {props.weatherInfo.location.name}, {props.weatherInfo.location.country}
       </div>
 
@@ -53,6 +35,6 @@ export const CurrentWeather = (props) => {
           </span>
         </div>
       </div>
-    </Paper>
+    </div>
   );
 };

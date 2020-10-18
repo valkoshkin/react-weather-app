f2d3856655883b65b001107b5a0cc9195afbe9fc
Valery Koshkin <valkoshkin99@gmail.com>
2020-10-18 16:26:44 +0400

    Вывод полученной от сервера актуальной информации о погоде

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CurrentWeather.css";
-import { LocationOnOutlined, WbSunnyOutlined } from "@material-ui/icons";
+import { LocationOnOutlined } from "@material-ui/icons";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Paper from "@material-ui/core/Paper";
 
@@ -17,7 +17,6 @@ const useStyles = makeStyles(() => ({
     maxWidth: "50%",
     cursor: "default",
     userSelect: "none",
-    lineBreak: "anywhere",
     "&:hover": {
       boxShadow: "0 1px 6px 2px rgba(0, 0, 0, 0.25)",
       transition: "box-shadow 100ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
@@ -29,22 +28,28 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-export const CurrentWeather = ({ ...props }) => {
+export const CurrentWeather = (props) => {
   const styles = useStyles();
 
   return (
     <Paper className={styles.root}>
       <div className="current-weather-location">
         <LocationOnOutlined className={styles.icon} fontSize="large" />
-        Los Angeles, USA
+        {props.weatherInfo.location.name}, {props.weatherInfo.location.country}
       </div>
 
       <div className="current-weather-info">
-        <WbSunnyOutlined className={styles.icon} fontSize="large" />
+        <img
+          className="current-weather-info-icon"
+          src={props.weatherInfo.current.condition.icon}
+          alt={props.weatherInfo.current.condition.text}
+        />
         <div className="current-weather-info-text">
-          <span className="current-weather-info-temperature">+13.7°C</span>
+          <span className="current-weather-info-temperature">
+            {props.weatherInfo.current.temp_c}°C
+          </span>
           <span className="current-weather-info-description">
-            Partly cloudly
+            {props.weatherInfo.current.condition.text}
           </span>
         </div>
       </div>
